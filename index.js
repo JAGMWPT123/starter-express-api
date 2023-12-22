dddfc0ae03c26d21258b09bcfb8a9477d5df0b8f
Marwan Mohammed <53322831+JAGMWPT123@users.noreply.github.com>
2023-12-22 20:42:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { createServer } = require('https');
+const { createServer } = require('node:https');
 //import http from 'node:http';
 const { Server } = require('socket.io');
 //const { setupWebSocket } = require('./socket.cjs');
